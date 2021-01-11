32e7f2d6937cec17738bd62d33b0d1537c2b5ce1
Alex Ruan <alexruan2222@gmail.com>
2021-01-11 14:04:34 -0500

    divided wound-analysis to frontend and backend repos
    Mainly because eventually this project will be given to a medical lab which likely does not have significant technical cs experience.
    
    Managing two heroku apps within a single git repo is possible but its more technically challenging.
    
    To make things easier for any lab member who wants to use this repo, i've divided it in two so that they don't have to deal with technical things like heroku buildpacks or git subtrees.

---
+++ src/components/MaskSelector/style.js
@@ -0,0 +1,18 @@
+const styles = {
+  textField: {
+    width: 140,
+    paddingRight: 20,
+  },
+  row: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "row",
+  },
+  column: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+  },
+};
+
+export default styles;
