c68f5a2cbd2ecab8d874b08abcf7521bd4052f86
Alex Ruan <alexruan2222@gmail.com>
2021-01-14 00:10:47 -0500

    heroku bugs: test with base index & app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
-import MainPage from "./components/MainPage";
-
-export default function App() {
+function App() {
   return (
-    <main>
-      <Switch>
-        <Route path="/" component={MainPage} exact />
-      </Switch>
-    </main>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-// You can choose your kind of history here (e.g. browserHistory)
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-import MainPage from "./components/MainPage";
-require("typeface-roboto-slab");
-// Your routes.js file
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" component={MainPage} exact />
-    </Switch>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
