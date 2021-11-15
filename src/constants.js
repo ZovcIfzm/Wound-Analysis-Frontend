657abd8a99f2a20f2cc10b1667dbdea96650071a
Alex Ruan <alexruan2222@gmail.com>
2021-11-15 00:43:22 -0500

    changed back to server url for api

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-//export const base_url = "https://gallagher-wound-analysis-api.herokuapp.com"
-export const base_url = "";
+export const base_url = "https://gallagher-wound-analysis-api.herokuapp.com";
+//export const base_url = "";
 export const base_ml_url = "https://gallagher-wound-analysis-ml.herokuapp.com";
